05eb940d9b056cf9ddac1ed3daa8e874713fbf14
Jackson Woodside <54115088+jdwoo090@users.noreply.github.com>
2019-12-12 08:58:13 -0700

    Add files via upload

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("blank1211321321")]
+[assembly: AssemblyTitle("ConSOLELLE")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("blank1211321321")]
+[assembly: AssemblyProduct("ConSOLELLE")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2328ee74-4a25-476d-a3da-3925cc67f193")]
+[assembly: Guid("b877b0d9-c836-4149-974d-c3a0bccba5a0")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Program.cs
+++ Program.cs
@@ -5,49 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
 
-namespace blank1211321321
+namespace ConSOLELLE
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //string aNumberValue;
-            string mula;
-            double euros;
-            double dollars;
-            //string ageValue;
-            string mlHeight;
-            int mileinch;
-            //int aNumber;
-            int mileInch;
-            //int age;
+            string answer = "";
 
-            //Write("Enter a number: ");
-            //aNumberValue = ReadLine();
-            //aNumber = int.Parse(aNumberValue);
-            //WriteLine("The square root of that number is " + Math.Sqrt(aNumber));
-            //Write("Now, enter your age: ");
-            //ageValue = ReadLine();
-            //age = int.Parse(ageValue);
-            //WriteLine("Your age next year will be {0} ", ++age);
-            Write("Enter your value in miles: ");
-            mlHeight = ReadLine();
-            mileinch = int.Parse(mlHeight);
-            mileInch = mileinch * 5280;
-            WriteLine("Your value in inches is: " + mileInch + "in");
-            Write("Please enter an amount in Euros: ");
-            mula = ReadLine();
-            euros = int.Parse(mula);
-            dollars = euros * 1.11;
-            WriteLine(euros + " euro is about " + dollars.ToString("C2"));
+            WriteLine("This program gets the average of a set of numbers, so how many numbers do you want to enter?");
+            answer = ReadLine();
+
+            double[] myDoubles = new double[int.Parse(answer)];
+            double sum = 0;
 
 
+            for (int i = 0; i < myDoubles.Length; i++)
+            {
+                WriteLine("Enter a number please");
+                myDoubles[i] = double.Parse(ReadLine());
+            }
 
-            Console.Read();
-        }
-        public static int inches(int feet)
-        {
-            return feet * 12;
+            foreach (double item in myDoubles)
+            {
+                sum = sum + item;
+            }
+
+            WriteLine("The average is {0:N2}", sum / int.Parse(answer));
+            ReadLine();
         }
     }
 }
