57b7b15f1d37f3690b71ba45f360d1d70be189a1
Jackson Woodside <54115088+jdwoo090@users.noreply.github.com>
2019-12-13 08:35:17 -0700

    Add files via upload

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Q3")]
+[assembly: AssemblyTitle("VSCQuestions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Q3")]
+[assembly: AssemblyProduct("VSCQuestions")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62167108-dd5c-4c94-aade-72d99ee511db")]
+[assembly: Guid("4b578cc9-2d5e-4a58-bbdf-53f9baca0ba0")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Program.cs
+++ Program.cs
@@ -5,26 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
 
-namespace Q3
+namespace VSCQuestions
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int height;
-            string heightN;
-            int width;
-            string widthN;
-            int areaF;
-            WriteLine("Please enter the height of a rectangle in inches");
-            heightN = ReadLine();
-            WriteLine("Please enter the width of a rectangle in inches");
-            widthN = ReadLine();
-            height = int.Parse(heightN);
-            width = int.Parse(widthN);
-            areaF = height * width;
-            WriteLine("The area of the rectangle is exactly " + areaF + " inches");
-            ReadKey();
+            string nameResponse;
+            string favoriteSaying;
+            string agreement;
+
+            WriteLine("Hello and welcome to our program! Before we begin, I need to warn you that this program will be asking for personal information like your name, are you okay with this? This information will not be saved or given away.");
+            agreement = ReadLine();
+            if (agreement.ToLower().Trim() == "no")
+            {
+                WriteLine("That is completely fine, and understandable! Please have a nice day and come back any time!");
+                ReadKey();
+            }
+            else
+            {
+                WriteLine("Okay, that's great! Then let's get right into it!");
+            WriteLine("What is your name?");
+            nameResponse = ReadLine();
+            WriteLine("I know this is a weird question, but do you have a favorite saying? For example, 'no pain no gain' or 'you miss 100% of the shots you don't take'. Do you have a favorite phrase that is something like that?");
+            favoriteSaying = ReadLine();
+                WriteLine("Okay! Thanks for your cooperation! This has been a lot of fun " + nameResponse + "! Remember, as the greats have always said " + favoriteSaying);
+                WriteLine("That is all, thank you!");
+                ReadKey();
+            }
         }
     }
 }
